895f1635b69176dfdcbb1ebab1befd62466913a5
Anish Karandikar <anish.karandikar@hotmail.com>
2019-01-17 22:38:25 -0500

    test: Add ability to use SERVICE_ACCOUNT_KEY_BASE64 env var

--- test/util.js
+++ test/util.js
@@ -1,8 +1,24 @@
+const fs = require('fs');
+
 module.exports = { getFirebaseCredentials, makeid, sleep };
 
 function getFirebaseCredentials() {
-  // TODO: Add ability to get credentials from ENV var
   const serviceAccountKeyFile = `${__dirname}/service-account-key.json`;
+
+  // If service account key file does not exist, get it from env var
+  if (!fs.existsSync(serviceAccountKeyFile)) {
+    const SERVICE_ACCOUNT_KEY_BASE64 = process.env.SERVICE_ACCOUNT_KEY_BASE64;
+    if (!SERVICE_ACCOUNT_KEY_BASE64) {
+      throw new Error(
+        'Either test/service-account-key.json or env var SERVICE_ACCOUNT_KEY_BASE64 must be provided.'
+      );
+    }
+    fs.writeFileSync(
+      serviceAccountKeyFile,
+      new Buffer(SERVICE_ACCOUNT_KEY_BASE64, 'base64').toString()
+    );
+  }
+
   return [
     { projectId: require(serviceAccountKeyFile).project_id },
     serviceAccountKeyFile,
